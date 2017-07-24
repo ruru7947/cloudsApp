0fa3d49a652ed0242d9a4823f21a093bc0857110
mike <chennanbang@gmail.com>
2017-07-24 13:55:31 +0800

    網路傳輸套件，初步測試

--- CloudsAPP/ViewController.swift
+++ CloudsAPP/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request as Any)
+            print(response.response as Any)
+            print(response.data as Any)
+            print(response.result)
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
